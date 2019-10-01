187e54de4141eef87dda494390f5cb338399aaa5
MJ <auriga@index.hu>
2019-10-01 21:00:47 +0200

    AS

---
+++ app/src/main/java/com/example/hunlang/Rules_Activity.java
@@ -0,0 +1,14 @@
+package com.example.hunlang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Rules_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules_);
+    }
+}
---
+++ app/src/main/java/com/example/hunlang/Together_Activity.java
@@ -0,0 +1,14 @@
+package com.example.hunlang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Together_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_together_);
+    }
+}
